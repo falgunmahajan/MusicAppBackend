c522a9b6bd25ddd225704d05a623095605e9adfd
Falgun-Mahajan-Avisoft <falgun.mahajan@avisoft.io>
2024-07-24 16:15:46 +0530

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SongsModule } from './songs/songs.module';
 import { AlbumsModule } from './albums/albums.module';
 
+
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017/MusicApp'), SongsModule, AlbumsModule],
   controllers: [AppController],
